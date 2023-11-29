26e50a973882d81a874e2952448ccc5c4ecb2ad3
liton61 <litonofficial61@gmail.com>
2023-11-29 20:01:47 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,6 +34,88 @@ async function run() {
         const trainerApplicationCollection = client.db("vectorGymDB").collection('trainerApplication');
         const photoCollection = client.db("vectorGymDB").collection('photo');
         const usersCollection = client.db("vectorGymDB").collection('users');
+
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middlewares 
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+        // use verify admin after verifyToken
+        // const verifyTrainer = async (req, res, next) => {
+        //     const email = req.decoded.email;
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query);
+        //     const isTrainer = user?.role === 'trainer';
+        //     if (!isTrainer) {
+        //         return res.status(403).send({ message: 'forbidden access' });
+        //     }
+        //     next();
+        // }
+
+
+        app.get('/users/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
+
+
+        // app.get('/users/trainer/:email', verifyToken, verifyTrainer, async (req, res) => {
+        //     const email = req.params.email;
+
+        //     if (email !== req.decoded.email) {
+        //         return res.status(403).send({ message: 'forbidden access' })
+        //     }
+
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query);
+        //     let trainer = false;
+        //     if (user) {
+        //         trainer = user?.role === 'trainer';
+        //     }
+        //     res.send({ trainer });
+        // })
 
 
         // post method added for subscriber
@@ -112,6 +195,15 @@ async function run() {
                     role: 'admin'
                 }
             }
+
+            // delete method to delete user
+            app.delete('/users/:id', async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) }
+                const result = await usersCollection.deleteOne(query);
+                res.send(result);
+            })
+
             const result = await usersCollection.updateOne(filter, updatedDoc);
             res.send(result);
         })
